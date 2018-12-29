95b26df88fa0b676b4913ea1c3216a1a3f8c86ac
Rohit Jagannath <rohitj559@gmail.com>
2018-12-28 20:53:43 -0600

    Update script.js

--- script.js
+++ script.js
@@ -106,6 +106,33 @@
 //   })}());
 
 // activity 5
+// (function(){
+//   angular.module('myApp', []).controller('myCtrl', function($scope, $http) {
+
+//     var onUserComplete = function(response){
+//       $scope.user = response.data
+//       $http.get($scope.user.repos_url)
+//       .then(onRepos, onError)
+//     };
+
+//     var onRepos = function(response){
+//       $scope.repos = response.data
+//       $scope.repoSortOrder = "-stargazers_count"
+//     };
+
+//     var onError = function(reason){
+//       $scope.error = "Could not fetch the data"
+//     };
+
+//     $scope.search = function(username){
+//       $http.get("https://api.github.com/users/" + username)
+//           .then(onUserComplete, onError);
+//     };
+
+//     $scope.message = "Github Viewer!!";
+//   })}());
+
+// activity 6
 (function(){
   angular.module('myApp', []).controller('myCtrl', function($scope, $http) {
 
